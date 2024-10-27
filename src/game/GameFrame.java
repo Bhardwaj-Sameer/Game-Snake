477519a76f3b80c96fe54d022c48508c69329eac
Bhardwaj-Sameer <saming.program@gmail.com>
2024-10-27 12:14:32 +0530

    Updated the icon on the GameFrame

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -3,13 +3,19 @@ package game;
 import javax.swing.JFrame;
 import game.GamePanel;
 
+import javax.swing.ImageIcon;
+
 public class GameFrame extends JFrame{
     public GameFrame(){
+
+        ImageIcon snake=new ImageIcon("src\\resources\\images\\snake.png");
         
         this.setTitle("Snake Game");
+        this.setIconImage(snake.getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(true);
         this.setSize(600,600);
+        
         
         GamePanel panel=new GamePanel();
         this.add(panel);
